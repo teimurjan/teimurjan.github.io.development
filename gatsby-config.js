262a5a05655ecf7f4015a3a7de04dfd4c1b69b41
Teimur Gasanov <teymurgg321@gmail.com>
2019-09-11 15:05:26 +0600

    Add gatsby-plugin-netlify-cms

--- gatsby-config.js
+++ gatsby-config.js
@@ -90,5 +90,11 @@ module.exports = {
       },
     },
     'gatsby-plugin-catch-links',
+    {
+      resolve: 'gatsby-plugin-netlify-cms',
+      options: {
+        modulePath: `${__dirname}/src/cms/cms.js`,
+      },
+    },
   ],
 }
