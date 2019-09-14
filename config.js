f01a16d46bb254c489e00b3ec019843bb1458fd6
Teimur Gasanov <teymurgg321@gmail.com>
2019-09-14 14:48:46 +0600

    Disable netlify plugin when it's not admin build

--- config.js
+++ config.js
@@ -6,5 +6,5 @@ module.exports = {
   GOOGLE_TAG_MANAGER_ID: process.env.GATSBY_GOOGLE_TAG_MANAGER_ID,
   IS_ADMIN_BUILD: process.env.GATSBY_IS_ADMIN_BUILD === 'true',
   // URL is a predefined Netlify variable
-  ADMIN_REDIRECT_URL: process.env.GATSBY_ADMIN_REDIRECT_URL
+  ADMIN_REDIRECT_URL: process.env.GATSBY_ADMIN_REDIRECT_URL,
 }
--- gatsby-config.js
+++ gatsby-config.js
@@ -93,7 +93,9 @@ module.exports = {
     {
       resolve: 'gatsby-plugin-netlify-cms',
       options: {
-        modulePath: `${__dirname}/src/cms/cms.js`,
+        modulePath: config.IS_ADMIN_BUILD
+          ? `${__dirname}/src/cms/cms.js`
+          : undefined,
       },
     },
   ],
